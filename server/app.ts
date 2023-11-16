bf60f199511a13e71a53a883ae290ab1dd98909e
saadsulmi <saadsulmi@gmail.com>
2023-11-16 21:08:20 +0530

    created basic server connection with database integration

---
+++ server/app.ts
@@ -0,0 +1,31 @@
+require('dotenv').config();
+import express, { NextFunction,Response,Request } from "express";
+export const app = express();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+// body parser
+app.use(express.json({limit:"50mb"}));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors set up
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+// test API
+app.get("/test",(req:Request,res:Response,next:NextFunction)=>{
+    res.status(200).json({
+        success: true,
+        message:"API working properly"
+    })
+})
+
+app.use('*',(req:Request,res:Response,next:NextFunction)=>{
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404
+    next(err)
+})
---
+++ server/utils/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+require('dotenv').config();
+
+const dbUrl:string = process.env.DB_URI|| process.env.DB_LOCAL||'' ;
+export const connectDB = async()=>{
+    try { 
+        await mongoose.connect(dbUrl).then((data:any):void=>{
+            console.log(`database connected to ${data.connection.host}`);
+            
+        })
+    } catch (error:any) {
+        console.log(error.message);
+        setTimeout(connectDB,5000)
+        
+    }
+}
